a127a0548f1cc857df7af7beaf025150d9a47fbf
Ben Tettmar <bentettmar@yahoo.com>
2022-06-25 15:47:48 +0100

    Added read more and read less for hire me section

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 const continueReading = document.getElementById('continue-reading');
 const continueReadingBtn = document.getElementById('continue-reading-btn');
 
+const continueReading2 = document.getElementById('continue-reading2');
+const continueReadingBtn2 = document.getElementById('continue-reading-btn2');
+
 function continueAboutMe() {
     continueReadingBtn.style.display = 'none';
     continueReading.style.display = 'block';
@@ -9,4 +12,14 @@ function continueAboutMe() {
 function hideAboutMe() {
     continueReadingBtn.style.display = 'block';
     continueReading.style.display = 'none';
+}
+
+function continueHireMe() {
+    continueReadingBtn2.style.display = 'none';
+    continueReading2.style.display = 'block';
+}
+
+function hideHireMe() {
+    continueReadingBtn2.style.display = 'block';
+    continueReading2.style.display = 'none';
 }
